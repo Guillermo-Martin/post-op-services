bbf1c129493cbb78454b67b310d0e247439a90da
Guillermo Martin <gscalica@gmail.com>
2025-09-08 17:42:41 -0700

    Fix email IDs in 'sendForm' function so owner gets notified when someone emails via the form, add success message for successful email, style message

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,5 +1,25 @@
 console.log("connected to contact.js!");
 
+const contactForm = document.getElementById("contact-us-form");
+const formContainer = document.querySelector(".form-container");
+// const submitButton = document.getElementById("submit-button");
+
+// submitButton.addEventListener("click", function(){
+//   event.preventDefault();
+//   // alert("connected!!");
+
+//   // hide the form
+//   // const contactForm = document.getElementById("contact-us-form");
+//   // contactForm.classList.add("hide");
+
+//   // in the div, add a message that says the message was sent.
+//   // get the form container
+//   const formContainer = document.querySelector(".form-container");
+//   const submitMessage = document.createElement("p");
+//   submitMessage.classList.add("submit-message", "active")
+//   submitMessage.textContent = "Thank you!  Your message has been sent.  A member our team will contact you soon.";
+//   formContainer.appendChild(submitMessage);
+// })
 
 
 (function() {
@@ -10,7 +30,7 @@ console.log("connected to contact.js!");
 })();
 
 window.onload = function() {
-  document.getElementById('contact-us-form').addEventListener('submit', function(event){
+  contactForm.addEventListener('submit', function(event){
       // prevent default form submission behavior
       event.preventDefault();
 
@@ -21,14 +41,28 @@ window.onload = function() {
       // set the hidden time input's time value
       document.getElementById("sent-time").value = formattedTime;
 
-      // these IDs from the previous steps: form to use and auto-reply template, respectively
-      emailjs.sendForm('contact_service', 'contact_form', this)
+      // "seviceId" is found in the EmailJS dashboard under "Email Services" and under the email service you want to use.
+      // "templateId" is found in the EmailJS dashboard under "Email Templates" and under the email template you want to use.
+      const serviceId = 'default_service';
+      const templateId = 'contact_service';
+
+      // send the info to EmailJS, who will then forward the info via email
+      emailjs.sendForm(serviceId, templateId, this)
         .then(() => {
             console.log('SUCCESS!');
-            alert("your message has been sent!")
+            
+            // hide the contact form
+            contactForm.classList.add("hide");
+
+            // create a "p" tag, give it some classes for styling, give it a success message, and append the "p" tag
+            // to the "formContainer" element
+            const submitMessage = document.createElement("p");
+            submitMessage.classList.add("submit-message", "active")
+            submitMessage.textContent = "Thank you!  Your message has been sent.  A member our team will contact you soon.";
+            formContainer.appendChild(submitMessage);
         }, (error) => {
             console.log('FAILED...', error);
-            alert("something went wrong.  try again later.")
+            // alert("something went wrong.  try again later.")
         });
   });
 };
