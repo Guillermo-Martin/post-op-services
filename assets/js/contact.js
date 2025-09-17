5cd10d34512fbf973219ced101fc2c73865fc412
Guillermo Martin <gscalica@gmail.com>
2025-09-17 16:28:39 -0700

    Remove form after submission

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,5 +1,6 @@
 const contactForm = document.getElementById("contact-us-form");
 const phoneInput = document.querySelector("input[name='phone']");
+const submitButton = document.getElementById("submit-button");
 
 const giveMessageFeedback = (str) => {
   // target the form container
@@ -43,43 +44,43 @@ phoneInput.addEventListener("input", function(event){
 });
 
 // ---------- Email JS ----------
-// (function() {
-//     // https://dashboard.emailjs.com/admin/account
-//     emailjs.init({
-//       publicKey: "jgcwgJAymXVkFay9x",
-//     });
-// })();
+(function() {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init({
+      publicKey: "jgcwgJAymXVkFay9x",
+    });
+})();
 
-// window.onload = function() {
-//   contactForm.addEventListener('submit', function(event){
-//     // prevent default form submission behavior
-//     event.preventDefault();
+window.onload = function() {
+  contactForm.addEventListener('submit', function(event){
+    // prevent default form submission behavior
+    event.preventDefault();
 
-//     // get the current date and time, and format it
-//     const sentTime = new Date();
-//     const formattedTime = sentTime.toLocaleString();
+    // get the current date and time, and format it
+    const sentTime = new Date();
+    const formattedTime = sentTime.toLocaleString();
 
-//     // set the hidden time input's time value
-//     document.getElementById("sent-time").value = formattedTime;
+    // set the hidden time input's time value
+    document.getElementById("sent-time").value = formattedTime;
 
-//     // "seviceId" is found in the EmailJS dashboard under "Email Services" and under the email service you want to use.
-//     // "templateId" is found in the EmailJS dashboard under "Email Templates" and under the email template you want to use.
-//     const serviceId = 'default_service';
-//     const templateId = 'contact_service';
+    // "seviceId" is found in the EmailJS dashboard under "Email Services" and under the email service you want to use.
+    // "templateId" is found in the EmailJS dashboard under "Email Templates" and under the email template you want to use.
+    const serviceId = 'default_service';
+    const templateId = 'contact_service';
 
-//     // send the info to EmailJS, who will then forward the info via email
-//     emailjs.sendForm(serviceId, templateId, this)
-//       .then(() => {
-//           // hide the contact form
-//           contactForm.classList.add("hide");
+    // send the info to EmailJS, who will then forward the info via email
+    emailjs.sendForm(serviceId, templateId, this)
+      .then(() => {
+          // hide the contact form
+          contactForm.classList.add("hide");
 
-//           // give success message feedback
-//           giveMessageFeedback("Thank you!  Your message has been sent.  A member our team will contact you soon.");
-//       }, (error) => {
-//           console.error('FAILED...', error);
+          // give success message feedback
+          giveMessageFeedback("Thank you!  Your message has been sent.  A member of our team will contact you soon.");
+      }, (error) => {
+          console.error('FAILED...', error);
 
-//           const errorMessage = document.querySelector(".error-message");
-//           errorMessage.classList.remove("hide");
-//       });
-//   });
-// };
+          const errorMessage = document.querySelector(".error-message");
+          errorMessage.classList.remove("hide");
+      });
+  });
+};
